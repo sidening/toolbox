568d85cd7c4363803fe16915294cd708a6d35dd9
SD-20181120DGNK\Administrator <1725185384@qq.com>
2019-10-25 17:17:30 +0800

    jsp

--- src/main/java/com/smilerro/toolbox/controller/cmsController.java
+++ src/main/java/com/smilerro/toolbox/controller/cmsController.java
@@ -1,6 +1,8 @@
 package com.smilerro.toolbox.controller;
 
+import com.smilerro.toolbox.entity.User;
 import com.smilerro.toolbox.entity.WebSite;
+import com.smilerro.toolbox.repository.UserRepository;
 import com.smilerro.toolbox.repository.WebSiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,14 +13,36 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/cms")
-public class cmsController {
+public class CmsController {
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     WebSiteRepository webSiteRepository;
-    @RequestMapping("/add")
-    public String add(Model model){
+    @RequestMapping("/login")
+    public String loginPage(Model model){
+        return "loginCms";
+    }
+    @RequestMapping("/userLogin")
+    public String userLogin(Model model, User user){
+        String username = user.getUsername();
+        String password = user.getPassword();
+        user = userRepository.findByUsernameAndPassword(username, password);
+        if (user!=null){
+            model.addAttribute("user",user);
+            return "list";
+        }else{
+            model.addAttribute("msg","login faild!");
+            return "loginCms";
+        }
+    }
+
+    @RequestMapping("/webList")
+    public String userLogin(Model model){
         List<WebSite> all = webSiteRepository.findAll();
-        model.addAttribute("webSites",all);
-        return "index";
+        if (all!=null){
+            model.addAttribute("webList",all);
+        }
+        return "list";
     }
 
 }
