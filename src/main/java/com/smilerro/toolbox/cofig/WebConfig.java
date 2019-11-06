a3ca166c365093882af3e6b6262b945c715e5328
Skf <shangkunfeng@123.com>
2019-11-06 18:18:31 +0800

    打包一版

---
+++ src/main/java/com/smilerro/toolbox/cofig/WebConfig.java
@@ -0,0 +1,19 @@
+package com.smilerro.toolbox.cofig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author ：Skf
+ * @date ：Created in 2019/11/6 0006 12:55
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
