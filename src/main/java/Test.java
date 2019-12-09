aedc7e1ee5f6bb78bdfe2d198cbc76e7a600ee96
root <18839@163.com>
2019-12-09 22:02:09 +0800

    ...

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * @author ：Skf
- * @date ：Created in 2019/11/13 0013 12:02
- */
-public class Test {
-}
--- src/main/java/com/smilerro/toolbox/ToolboxApplication.java
+++ src/main/java/com/smilerro/toolbox/ToolboxApplication.java
@@ -2,13 +2,18 @@ package com.smilerro.toolbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToolboxApplication  {
+public class ToolboxApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
         SpringApplication.run(ToolboxApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ToolboxApplication.class);
+    }
 }
